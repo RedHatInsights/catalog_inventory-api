bf8d0885f87dc734ec153231287c9bacefe0f27f
hsong-rh <hsong@redhat.com>
2021-05-13 15:14:36 -0400

    Replace after_update with after_commit

--- app/models/full_refresh_persister_task.rb
+++ app/models/full_refresh_persister_task.rb
@@ -1,5 +1,5 @@
 class FullRefreshPersisterTask < KafkaMessageTask
-  after_update :post_persister_task, :if => proc { state == 'completed' }
+  after_commit :post_persister_task, :if => proc { state == 'completed' }
 
   def post_persister_task
     PostPersisterTaskService.new(service_options).process
