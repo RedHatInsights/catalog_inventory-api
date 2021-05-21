a393f8147ac3a19b23bcf05b19838697713d1bf3
hsong-rh <hsong@redhat.com>
2021-05-21 10:53:44 -0400

    Add more log messages for debugging

--- app/models/ingress_payload.rb
+++ app/models/ingress_payload.rb
@@ -2,11 +2,14 @@ class IngressPayload < ApplicationRecord
   after_create_commit :check_pending_upload_tasks
 
   def check_pending_upload_tasks
-    if Task.find(task_id).state == 'completed'
-      Rails.logger.info("Task #{task_id} is completed, start persister task service")
-      self.class.delete(id)
+    task = Task.find(task_id)
+    Rails.logger.info("Task: #{task.id}, #{task.state}")
 
+    if task.state == 'completed'
+      Rails.logger.info("Task #{task_id} is completed, start persister task service")
       PersisterTaskService.new(payload).process
+
+      self.class.delete(id)
     else
       Rails.logger.info("Ingress Kafka Message comes first")
     end
